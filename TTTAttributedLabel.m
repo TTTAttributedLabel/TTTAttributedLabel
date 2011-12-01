963925340beee9021ec50ad1783da4aa1a940a51
Pete Callaway <pete@dativestudios.com>
2011-12-01 15:12:46 +0000

    If the numberOfLines is 1, sizeThatFits returns the full required width of the text

--- TTTAttributedLabel.m
+++ TTTAttributedLabel.m
@@ -528,9 +528,14 @@ static inline NSAttributedString * NSAttributedStringByScalingFontSize(NSAttribu
     }
     
     CFRange rangeToSize = CFRangeMake(0, [self.attributedText length]);
+    CGSize constraints = CGSizeMake(size.width, CGFLOAT_MAX);
     
-    // If the line count of the label isn't 0, limit the range to size to the number of lines that have been set
-    if (self.numberOfLines > 0) {
+    if (self.numberOfLines == 1) {
+        // If there is one line, the size that fits is the full width of the line
+        constraints = CGSizeMake(CGFLOAT_MAX, CGFLOAT_MAX);
+    }
+    else if (self.numberOfLines > 0) {
+        // If the line count of the label more than 1, limit the range to size to the number of lines that have been set
         CGMutablePathRef path = CGPathCreateMutable();
         CGPathAddRect(path, NULL, CGRectMake(0, 0, self.bounds.size.width, CGFLOAT_MAX));
 
@@ -549,7 +554,7 @@ static inline NSAttributedString * NSAttributedStringByScalingFontSize(NSAttribu
         CFRelease(path);
     }
     
-    CGSize suggestedSize = CTFramesetterSuggestFrameSizeWithConstraints(self.framesetter, rangeToSize, NULL, CGSizeMake(size.width, CGFLOAT_MAX), NULL);
+    CGSize suggestedSize = CTFramesetterSuggestFrameSizeWithConstraints(self.framesetter, rangeToSize, NULL, constraints, NULL);
     return CGSizeMake(ceilf(suggestedSize.width), ceilf(suggestedSize.height));
 }
 
