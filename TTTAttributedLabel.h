8c9d5a88b803394a6553cfdf9a8f1189fc8faaa7
Pete Callaway <pete@dativestudios.com>
2011-12-07 22:39:34 +0800

    Add truncateLastLine property to allow truncation of attributed strings that don't fit a label with a maximum number of lines

--- TTTAttributedLabel.h
+++ TTTAttributedLabel.h
@@ -157,6 +157,10 @@ typedef enum {
  */
 @property (nonatomic, assign) TTTAttributedLabelVerticalAlignment verticalAlignment;
 
+/**
+ A Boolean value that determines whether the last line of the `NSAttributedString` attribute should be trunctated. This property is effective for any value of `numberOfLines` greater than zero
+ */
+@property (nonatomic, assign) BOOL truncateLastLine;
 
 ///----------------------------------
 /// @name Setting the Text Attributes
