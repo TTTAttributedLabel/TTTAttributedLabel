88f0049968dfd1bece3a77d254fbcdcde99728f1
Jesse Collis <jesse@jcmultimedia.com.au>
2013-11-14 16:53:46 +0800

    use MAX rather than fmax to avoid warnings on 64bit

--- TTTAttributedLabel/TTTAttributedLabel.m
+++ TTTAttributedLabel/TTTAttributedLabel.m
@@ -980,7 +980,7 @@ afterInheritingLabelAttributesAndConfiguringWithBlock:(NSMutableAttributedString
     CGRect textRect = bounds;
 
     // Calculate height with a minimum of double the font pointSize, to ensure that CTFramesetterSuggestFrameSizeWithConstraints doesn't return CGSizeZero, as it would if textRect height is insufficient.
-    textRect.size.height = fmaxf(self.font.pointSize * 2.0f, bounds.size.height);
+    textRect.size.height = MAX(self.font.pointSize * 2.0f, bounds.size.height);
 
     // Adjust the text to be in the center vertically, if the text size is smaller than bounds
     CGSize textSize = CTFramesetterSuggestFrameSizeWithConstraints([self framesetter], CFRangeMake(0, (CFIndex)[self.attributedText length]), NULL, textRect.size, NULL);
