af85eb094ca70484b96353b810c9b5e80a1c8d09
sivawang <SivaWang@siva.local>
2014-04-08 11:01:35 +0800

    avoid crash if NSLinkAttributeName didn’t exist

--- TTTAttributedLabel/TTTAttributedLabel.m
+++ TTTAttributedLabel/TTTAttributedLabel.m
@@ -981,12 +981,14 @@ static inline CGSize CTFramesetterSuggestFrameSizeForAttributedStringWithConstra
     }
 
 #if __IPHONE_OS_VERSION_MAX_ALLOWED >= 70000
-    [self.attributedText enumerateAttribute:NSLinkAttributeName inRange:NSMakeRange(0, self.attributedText.length) options:0 usingBlock:^(id value, __unused NSRange range, __unused BOOL *stop) {
-        if (value) {
-            NSURL *URL = [value isKindOfClass:[NSString class]] ? [NSURL URLWithString:value] : value;
-            [self addLinkToURL:URL withRange:range];
-        }
-    }];
+    if (&NSLinkAttributeName){ //Avoid crash on iOS 5
+        [self.attributedText enumerateAttribute:NSLinkAttributeName inRange:NSMakeRange(0, self.attributedText.length) options:0 usingBlock:^(id value, __unused NSRange range, __unused BOOL *stop) {
+            if (value) {
+                NSURL *URL = [value isKindOfClass:[NSString class]] ? [NSURL URLWithString:value] : value;
+                [self addLinkToURL:URL withRange:range];
+            }
+        }];
+    }
 #endif
 
     [super setText:[self.attributedText string]];
