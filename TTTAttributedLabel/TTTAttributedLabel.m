08dc33728582684918a60a0cf62bc318bce40179
Hermes Pique <hermespique@gmail.com>
2014-11-20 14:48:13 +0000

    Avoid recreating the data detector

--- TTTAttributedLabel/TTTAttributedLabel.m
+++ TTTAttributedLabel/TTTAttributedLabel.m
@@ -546,6 +546,10 @@ static inline CGSize CTFramesetterSuggestFrameSizeForAttributedStringWithConstra
 }
 
 - (void)setEnabledTextCheckingTypes:(NSTextCheckingTypes)enabledTextCheckingTypes {
+    // Avoid recreating the data detector
+    if (_enabledTextCheckingTypes == enabledTextCheckingTypes) {
+        return;
+    }
     _enabledTextCheckingTypes = enabledTextCheckingTypes;
 
     if (self.enabledTextCheckingTypes) {
