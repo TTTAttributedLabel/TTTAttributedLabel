7b9c5d1061cedc4cd49bc25fcb9872dce0487290
Jonathan Hersh <jon@her.sh>
2015-06-15 16:50:19 -0700

    [Tests] add test-only app delegate/root VC

---
+++ Example/AppTestDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppTestDelegate.h
+//  Espressos
+//
+//  Created by Jonathan Hersh on 6/15/15.
+//  Copyright (c) 2015 Gowalla. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppTestDelegate : UIResponder <UIApplicationDelegate>
+
+@property (nonatomic, strong) UIWindow *window;
+
+@end
