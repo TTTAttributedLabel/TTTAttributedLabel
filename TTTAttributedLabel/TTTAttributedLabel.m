b4a366b913189e55cb7098ea2df15aba7e1da373
Alexey Ereschenko <ereshchenko@gmail.com>
2015-09-24 08:52:19 +0300

    Suppress warning 'address will always eveluate to true' for iOS9 SDK

--- TTTAttributedLabel/TTTAttributedLabel.m
+++ TTTAttributedLabel/TTTAttributedLabel.m
@@ -1175,6 +1175,8 @@ static inline CGSize CTFramesetterSuggestFrameSizeForAttributedStringWithConstra
     }
 
 #if __IPHONE_OS_VERSION_MAX_ALLOWED >= 70000
+#pragma clang diagnostic push
+#pragma clang diagnostic ignored "-Waddress"
     if (&NSLinkAttributeName) {
         [self.attributedText enumerateAttribute:NSLinkAttributeName inRange:NSMakeRange(0, self.attributedText.length) options:0 usingBlock:^(id value, __unused NSRange range, __unused BOOL *stop) {
             if (value) {
@@ -1183,6 +1185,7 @@ static inline CGSize CTFramesetterSuggestFrameSizeForAttributedStringWithConstra
             }
         }];
     }
+#pragma clang diagnostic pop
 #endif
 }
 
