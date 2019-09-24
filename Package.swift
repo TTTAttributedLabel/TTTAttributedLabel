0834227073a8aebab3ea25004707028e6018344f
Petteri Hyttinen <petteri.hyttinen@gmail.com>
2019-09-24 10:04:49 +0200

    Add objc dependency

--- Package.swift
+++ Package.swift
@@ -20,7 +20,7 @@ let package = Package(
         // Targets can depend on other targets in this package, and on products in packages which this package depends on.
         .target(
             name: "TTTAttributedLabel",
-            dependencies: []),
+            dependencies: ["TTTAttributedLabel"]),
         .testTarget(
             name: "TTTAttributedLabelTests",
             dependencies: ["TTTAttributedLabel"]),
