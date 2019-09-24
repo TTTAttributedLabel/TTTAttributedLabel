e25cb9ea82c357dbd2e6d5119346a694818e724b
Petteri Hyttinen <petteri.hyttinen@gmail.com>
2019-09-24 09:49:52 +0200

    Generated Package.swift

---
+++ Tests/TTTAttributedLabelTests/TTTAttributedLabelTests.swift
@@ -0,0 +1,15 @@
+import XCTest
+@testable import TTTAttributedLabel
+
+final class TTTAttributedLabelTests: XCTestCase {
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct
+        // results.
+        XCTAssertEqual(TTTAttributedLabel().text, "Hello, World!")
+    }
+
+    static var allTests = [
+        ("testExample", testExample),
+    ]
+}
