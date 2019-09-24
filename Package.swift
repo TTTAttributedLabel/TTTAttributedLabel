6bfd5762d585c78ac81049fba71657fe5378496a
Petteri Hyttinen <petteri.hyttinen@gmail.com>
2019-09-24 10:16:11 +0200

    no message

--- Package.swift
+++ Package.swift
@@ -20,9 +20,7 @@ let package = Package(
         // Targets can depend on other targets in this package, and on products in packages which this package depends on.
         .target(
             name: "TTTAttributedLabel",
-            path: ".",
             exclude: [],
-            sources: ["Source", "TTTAttributedLabel.m"],
-            publicHeadersPath: "Source"),
+            sources: ["TTTAttributedLabel.m"],
     ]
 )
