1b6814f5eeda2ea36168277cdfec99f11ea9ed4e
Nikita Konopelko <nik.sativa@gmail.com>
2022-02-20 11:25:46 +0200

    SPM support

---
+++ Package.swift
@@ -0,0 +1,14 @@
+// swift-tools-version:5.3
+import PackageDescription
+
+let package = Package(
+    name: "TTTAttributedLabel",
+    platforms: [.iOS(.v13)],
+    products: [
+        .library(name: "TTTAttributedLabel", targets: ["TTTAttributedLabel"])
+    ],
+    targets: [
+        .target(name: "TTTAttributedLabel",
+                path: "TTTAttributedLabel")
+    ]
+)
